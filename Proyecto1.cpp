d082383487d4e69c09f09a6c93446cfbc7169d78
VeraSupremo <martiniverinirx@gmail.com>
2024-05-04 00:24:45 -0400

    Update Proyecto1.cpp

--- Proyecto1.cpp
+++ Proyecto1.cpp
@@ -23,8 +23,6 @@ void processInput(GLFWwindow *windows, Circulo circulo,Ventana ventana)
 
 void framebuffer_size_callback(GLFWwindow* windows, int width, int height)
 {
-    // make sure the viewport matches the new window dimensions; note that width and 
-    // height will be significantly larger than specified on retina displays.
     glViewport(0, 0,width,height);
 }
 float RC(){
