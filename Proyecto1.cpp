5c40a3fe867d159362b09a6e380db3e07e49a5ff
VeraSupremo <martiniverinirx@gmail.com>
2024-05-07 01:24:02 -0400

    s

--- Proyecto1.cpp
+++ Proyecto1.cpp
@@ -87,14 +87,14 @@ int main(){
     }
     while (!glfwWindowShouldClose(window))
     {
-        // input
+        
         processInput(window,circulo,ventana);
 
-        // render
+        // renderizado del circulo
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
+        
         Pintar_Circulo(circulo);
         glfwSwapBuffers(window);
         glfwPollEvents();
